c74421a56aaa031cc94cf13b6e4928de271edd3e
TimotyDarrelDavidson <c14230235@john.petra.ac.id>
2025-03-12 20:10:51 +0700

    fixed issue

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,6 +1,6 @@
 <div class="w-full bg-gray-900 ">
     <header class="lg:px-16 px-4 flex flex-wrap items-center py-4 shadow-lg">
-        <div class="hidden md:flex md:items-center md:w-auto w-full" id="menu">
+        <div class="md:flex md:items-center md:w-auto w-full" id="menu">
             <nav>
                 <ul class="md:flex items-center justify-between text-base text-white pt-4 md:pt-0">
                     <li><a class="md:p-4 py-3 px-0 block text-rose-500" href="/">Home</a></li>
